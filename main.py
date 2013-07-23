8e6d405659a9a42cd11af9ff7f9e5a9ba9bae740
L8D <tenorbiel@gmail.com>
2013-07-23 01:05:09 -0500

    FOR CONSISTANCY!

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ data = json.loads(config_file.read())
 for player in data['players']:
   print(player['name'] + " is level " + str(player['level']))
 
-gen_data = json.dumps(data, indent=2, separators=(',', ': '))
+gen_data = json.dumps(data, sort_keys=True, indent=2, separators=(',', ': '))
 
 config_file.seek(0)
 config_file.write(gen_data)

bbb13c41044c1814c43f5a8cf67e092dfa0b4e0b
L8D <tenorbiel@gmail.com>
2013-07-23 00:58:59 -0500

    Changed for better clarity

--- main.c
+++ main.c
@@ -2,16 +2,16 @@
 #include "lib/parson.c"
 
 int main() {
-  char filename[] = "data.json";
-  JSON_Value *data = json_parse_file(filename);
+  JSON_Value *data = json_parse_file("data.json");
   JSON_Array *players = json_object_get_array(json_value_get_object(data), "players");
   JSON_Object *player;
 
   for(size_t i = 0; i < json_array_get_count(players); i++) {
     player = json_array_get_object(players, i);
-    printf("%s is level %d\n",
+
+    printf("%s is level %.0f\n",
         json_object_get_string(player, "name"),
-        (int)json_object_get_number(player, "level")
+        json_object_get_number(player, "level")
         );
   }
 
